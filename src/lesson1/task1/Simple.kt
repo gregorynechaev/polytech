86d602fd8405b4e40a1feb79659b4f8a3bfe9981
Gregory <mpika1@yandex.ru>
2022-09-07 19:22:48 +0300

    test

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -74,7 +74,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = seconds + 60*minutes 
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = sagenes*48*0.04445+arshins*16*0.04445+vershoks*0.0445
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = sagenes*48*0.04445 + arshins*16*0.04445 + vershoks*0.0445
 
 /**
  * Тривиальная (1 балл)
@@ -82,7 +82,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = sagenes*
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = deg*0.017+min*0.00029+sec*0.0000048
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = deg*0.017 + min*0.00029 + sec*0.0000048
 
 /**
  * Тривиальная (1 балл)
@@ -90,7 +90,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = deg*0.017+min*0.00029+
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqr(abs(x2-x1))+sqr(abs(y2-y1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqr(abs(x2-x1)) + sqr(abs(y2-y1))
 
 /**
  * Простая (2 балла)
@@ -107,7 +107,13 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    val minutedep = hoursDepart*60+minutesDepart
+    val minutearr = hoursArrive*60+minutesArrive
+    return minutearr-minutedep
+
+
+}
 
 /**
  * Простая (2 балла)
